7236c5c2073a2924ce905477bc4cb9ce83454c28
tolihongjing <tolihongjing@gmail.com>
2013-11-26 11:40:29 +0800

    create project

---
+++ src/main/ImmutableListExample.java
@@ -0,0 +1,14 @@
+package main;
+import com.google.common.collect.ImmutableList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 13-11-26
+ * Time: 上午11:32
+ * To change this template use File | Settings | File Templates.
+ */
+public class ImmutableListExample {
+    public ImmutableList<String> of;
+
+}
