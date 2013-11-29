3e8a543961ff91dfdca9fa5ca304eb0f783d468a
tolihongjing <tolihongjing@gmail.com>
2013-11-29 15:31:57 +0800

    update with filter() transform() and find()

--- src/main/ImmutableListExample.java
+++ src/main/ImmutableListExample.java
@@ -1,10 +1,15 @@
 package main;
 
+import com.google.common.base.Function;
+import com.google.common.base.Predicate;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 /**
  * Created with IntelliJ IDEA.
@@ -18,32 +23,48 @@ public class ImmutableListExample {
     public ImmutableList<String> innerList = ImmutableList.of("1", "2", "3", "4", "5", "6", "7", "8", "9", "10");
 
     public List getOdd() {
-        List<String> retList = new ArrayList<String>();
-        for (int i = 0; i < innerList.size(); i++)
-            if (Integer.valueOf(innerList.get(i)) % 2 != 0)
-                retList.add(innerList.get(i));
-        return retList;
+        Iterable<String> filtered = Iterables.filter(innerList, new Predicate<String>() {
+            @Override
+            public boolean apply(@Nullable java.lang.String s) {
+                if (Integer.valueOf(s) % 2 == 1)
+                    return true;
+                else
+                    return false;
+            }
+        });
+        return ImmutableList.copyOf(filtered);
     }
 
-    public void updateEven()
-    {
-        List<String> retList = new ArrayList(innerList.asList());
-        for(int i=0; i < retList.size(); i++)
-        {
-            String temp = retList.get(i);
-            Integer j = Integer.valueOf(temp);
-            if( j % 2 == 0)
-                temp = String.valueOf(2 * j);
-            retList.set(i, temp);
-        }
-        innerList = ImmutableList.copyOf(retList.iterator());
+    public List updateEven() {
+        List converted = ImmutableList.copyOf(
+                Iterables.transform(innerList, new Function() {
+                    @Override
+                    public Object apply(@Nullable java.lang.Object o) {
+                        if (String.class == o.getClass()
+                                && Integer.valueOf((String) o) % 2 == 0) {
+                            o = String.valueOf(2 * Integer.valueOf((String) o));
+                        }
+                        return o;
+                    }
+                })
+        );
+        return converted;
     }
 
-    public String check(int in)
-    {
-        if(innerList.contains(String.valueOf(in)))
-            return "Hello, "+in;
-        else
+    public String check(final int in) {
+        try {
+            String str = Iterables.find(innerList, new Predicate<String>() {
+                @Override
+                public boolean apply(@Nullable java.lang.String s) {
+                    if (s.equals(String.valueOf(in)))
+                        return true;
+                    else
+                        return false;
+                }
+            });
+            return "Hello, " + str;
+        } catch (NoSuchElementException e) {
             return "Not Found";
+        }
     }
 }
--- src/test/ImmutableListTest.java
+++ src/test/ImmutableListTest.java
@@ -1,12 +1,9 @@
 package test;
 
 import com.google.common.collect.ImmutableList;
-
 import main.ImmutableListExample;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import static org.hamcrest.core.Is.is;
@@ -22,36 +19,25 @@ import static org.junit.Assert.assertThat;
 public class ImmutableListTest {
 
     @Test
-    public void getOddTest()
-    {
+    public void getOddTest() {
         ImmutableListExample example = new ImmutableListExample();
         List<String> oddList = example.getOdd();
-        assertThat(oddList.contains("1"), is(true));
-        assertThat(oddList.contains("2"), is(false));
-        assertThat(oddList.contains("5"), is(true));
-        assertThat(oddList.contains("7"), is(true));
-        assertThat(oddList.contains("11"), is(false));
+        assertThat(oddList.equals(ImmutableList.of("1", "3", "5", "7", "9")), is(true));
     }
 
     @Test
-    public void updateEvenTest()
-    {
+    public void updateEvenTest() {
         ImmutableListExample example = new ImmutableListExample();
-        example.updateEven();
-        ImmutableList other = ImmutableList.of("1", "4", "3", "8", "5", "12", "7", "16", "9", "20");
-        for(int i = 0; i < other.size(); i++)
-            assertThat(example.innerList.get(i).equals(other.get(i)), is(true));
+        assertThat(example.updateEven().equals(ImmutableList
+                .of("1", "4", "3", "8", "5", "12", "7", "16", "9", "20")), is(true));
 
     }
+
     @Test
-    public void checkTest()
-    {
+    public void checkTest() {
         ImmutableListExample example = new ImmutableListExample();
-        assertThat(example.check(1).equals("Hello, "+1), is(true));
-        assertThat(example.check(3).equals("Hello, "+3), is(true));
-        assertThat(example.check(5).equals("Hello, "+5), is(true));
-        assertThat(example.check(7).equals("Hello, "+7), is(true));
-        assertThat(example.check(9).equals("Hello, "+9), is(true));
+        assertThat("Not Found".equals(example.check(14)), is(true));
+        assertThat("Hello, 1".equals(example.check(1)), is(true));
     }
 
 }
